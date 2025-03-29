ca2e1c2043871c277e14ce4c2658a14788b18e54
LuisWork <luiszambrano.work@gmail.com>
2025-03-29 19:02:14 -0300

    Implementando Clase Entity Product

---
+++ src/main/java/com/luiszambrano/backend/cartapp/spring_1_cartapp/Spring1CartappApplication.java
@@ -0,0 +1,13 @@
+package com.luiszambrano.backend.cartapp.spring_1_cartapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1CartappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1CartappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luiszambrano/backend/cartapp/spring_1_cartapp/Spring1CartappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiszambrano.backend.cartapp.spring_1_cartapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1CartappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
