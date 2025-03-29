2c07abfd794f0bcf0afb34f7c28d68357b72b628
LuisWork <luiszambrano.work@gmail.com>
2025-03-29 19:30:49 -0300

    Eliminando ApplicationProperties del repo

---
+++ src/main/java/com/luiszambrano/backend/cartapp/spring_1_cartapp/controllers/ProductController.java
@@ -0,0 +1,25 @@
+package com.luiszambrano.backend.cartapp.spring_1_cartapp.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luiszambrano.backend.cartapp.spring_1_cartapp.models.entities.Product;
+import com.luiszambrano.backend.cartapp.spring_1_cartapp.services.ProductService;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/products")
+    public List<Product> list() {
+        return service.findAll();
+    }
+
+}
--- src/main/java/com/luiszambrano/backend/cartapp/spring_1_cartapp/services/ProductServiceImpl.java
+++ src/main/java/com/luiszambrano/backend/cartapp/spring_1_cartapp/services/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.luiszambrano.backend.cartapp.spring_1_cartapp.models.entities.Product;
 import com.luiszambrano.backend.cartapp.spring_1_cartapp.repositories.ProductRepository;
@@ -15,6 +16,7 @@ public class ProductServiceImpl implements ProductService{
     private ProductRepository repository;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Product> findAll() {
        return (List<Product>) repository.findAll();
     }
