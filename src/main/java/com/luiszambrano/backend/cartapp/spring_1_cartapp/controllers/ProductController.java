f3dcae4c2c7b83abeeebfbc5d2643bfd77e79a6a
LuisWork <luiszambrano.work@gmail.com>
2025-04-01 19:25:36 -0300

    Implementando CrossOrigins en el Controller para el consumo de la API

--- src/main/java/com/luiszambrano/backend/cartapp/spring_1_cartapp/controllers/ProductController.java
+++ src/main/java/com/luiszambrano/backend/cartapp/spring_1_cartapp/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.luiszambrano.backend.cartapp.spring_1_cartapp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.luiszambrano.backend.cartapp.spring_1_cartapp.services.ProductService
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
 
     @Autowired
